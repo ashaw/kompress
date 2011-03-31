880f9438f94e2dc8e772160ab22b86fc60cb6651
Al Shaw <almshaw@gmail.com>
2011-03-31 10:17:50 -0400

    readme

---
+++ README.markdown
@@ -0,0 +1,14 @@
+# Kompress
+
+A simple CSS compressor. It removes `\s*`.
+
+## Usage
+
+    k = Kompress.new(big_css)
+    k.css #=> compressed css
+    
+# Copyright
+
+Copyright (c) 2011 Al Shaw. See LICENSE.txt for
+further details.
+
--- lib/kompress.rb
+++ lib/kompress.rb
@@ -2,5 +2,5 @@ class Kompress
   ROOT = File.expand_path "#{File.dirname __FILE__}/.."
 end
 
-require "#{TimelineSetter::ROOT}/lib/kompress/version"
-require "#{TimelineSetter::ROOT}/lib/kompress/kompress"
+require "#{Kompress::ROOT}/lib/kompress/version"
+require "#{Kompress::ROOT}/lib/kompress/kompress"
